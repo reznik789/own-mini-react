15fd4c08c859b360bca0946fea15afe17cbbfaf8
Alex Reznichenko <alex.r@itmaster-soft.com>
2020-08-01 15:51:42 +0300

    Render and Commit Phases

--- src/Didact.js
+++ src/Didact.js
@@ -22,25 +22,95 @@ function createElement(type, props, ...children) {
   };
 }
 
-function render(element, container) {
+function createDom(fiber) {
   const dom =
-    element.type === TEXT_ELEMENT
+    fiber.type === TEXT_ELEMENT
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
   const isProp = key => key !== "children";
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProp)
     .forEach(prop => {
-      dom[prop] = element.props[prop];
+      dom[prop] = fiber.props[prop];
     });
-  console.log(element);
-
-  element.props.children.forEach(child => {
-    render(child, dom);
-  });
-  container.appendChild(dom);
+  
+    return dom;
 }
+
+let nextUnitOfWork = null;
+let wipRoot = null;
+
+function performUnitOfWork(fiber) {
+  //add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+  //create new fibers
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index ++;
+  } 
+  // return new unit of work 
+  if (fiber.child) {
+    return fiber.child;
+  } 
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+}
+
+function commitRoot() {
+  // TODO add nodes to dom
+}
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork
+    )
+    shouldYield = deadline.timeRemaining() < 1
+  }
+
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot()
+  }
+
+  requestIdleCallback(workLoop)
+}
+requestIdleCallback(workLoop);
+
+function render(element, container) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  }
+  nextUnitOfWork = wipRoot;
+}
+
 
 const Didact = {
   createElement,
